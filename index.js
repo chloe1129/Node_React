31ec0a2264fbd1f99c9e34fc6750f587262b590b
Chaehy <chlee7670@cau.ac.kr>
2023-03-14 20:52:35 -0700

    lecture 03

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express(); // new express app
 const port = 3000; // my backend server port
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://chloe:a1234@atlascluster.vwk8mbf.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useCreateIndex: true,
+      //useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB connected!!"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello World! 나는 이채현");
 });
